6138e2533c6c202bdd398000e18f9b0e3f6955c9
rafael-mqueiroz <rafael.queiroz@serttel.com.br>
2018-12-18 14:03:46 -0300

    add pug serve it

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
+app.set('view engine', 'pug')
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -13,9 +14,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.route('/')
   .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
+    res.render(process.cwd() + '/views/pug/index.pug');
   });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(/*process.env.PORT ||*/ 3000, () => {
   console.log("Listening on port " + process.env.PORT);
 });
